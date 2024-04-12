0708d1837d00e0544272b758dcd4069c29511b1a
Paul Kroeger <paulkroeger98@gmail.com>
2024-04-12 16:59:56 +0200

    added horizontal symmetry

--- experimentation/revolve2/experimentation/symmetry_measures/symmetry_measures.py
+++ experimentation/revolve2/experimentation/symmetry_measures/symmetry_measures.py
@@ -35,7 +35,6 @@ def calculate_vertical_symmetry(body : Body) -> float:
                 number_of_modules += 1
             elif grid[i][j] is not None:
                 number_of_modules += 1
-    print(number_of_modules)
     #Calculate symmetry
     symmetrical_nodes = 0
     for y in range(0,height):
@@ -51,11 +50,45 @@ def calculate_vertical_symmetry(body : Body) -> float:
             number_of_modules -= 1    
     if number_of_modules == 0 :
         return 1.0
-    print(grid)
-    print(symmetrical_nodes)
-    print(number_of_modules)
+
     return symmetrical_nodes/number_of_modules
 
+def calculate_horizontal_symmetry(body : Body) -> float:
+    grid = to_grid(cast(CoreNode, Node.from_module(body.core)))
+    width = len(grid[0])
+    height = len(grid)
+    # [core_position] = [
+    #     (x, y)
+    #     for x, node in x enumarate(xs)
+    #     for y, xs in enumarate(grid)
+    #     if isinstance(node, CoreNode)
+    # ] find below in readable
+    core_position = (0,0)
+    number_of_modules = 0
+    for i in range(0,height):
+        for j in range(0,width):
+            if isinstance(grid[i][j],CoreNode):
+                core_position = (i,j)
+                number_of_modules += 1
+            elif grid[i][j] is not None:
+                number_of_modules += 1
+    #Calculate symmetry
+    symmetrical_nodes = 0
+    for x in range(0,width):
+        for y in range(1,(height-1)//2 + 1):
+            if core_position[0]-y < 0 or core_position[0] + y >= height:
+                pass
+            else:
+                if grid[core_position[0]-y][x] is not None and type(
+                grid[core_position[0]-y][x]) == type(grid[core_position[0]+y][x]):
+                    symmetrical_nodes += 2
+    #remove modules in axis of core as they dont count for the symmetry measure
+        if grid[core_position[0]][x] is not None:
+            number_of_modules -= 1    
+    if number_of_modules == 0 :
+        return 1.0
+
+    return symmetrical_nodes/number_of_modules
                     
 
 
