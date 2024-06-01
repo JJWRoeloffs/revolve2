401ff1bde990cbaae64fbe9aaa7bd5958c825ae6
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-06-01 15:59:50 +0200

    Fix genotype assertion fail.
    Sometimes, the stuff passes an int instead of a float,
    so, instead of asserting everything _is_ a float,
    the code now tries to convert everything to a float,
    crashing if it fails.

--- experimentation/revolve2/experimentation/genotypes/grn/genotype.py
+++ experimentation/revolve2/experimentation/genotypes/grn/genotype.py
@@ -3,6 +3,4 @@ from typing import List
 
 class Genotype:
     def __init__(self, genotype: List[float]):
-        assert isinstance(genotype, List)
-        assert all(isinstance(x, float) for x in genotype)
-        self.genotype = genotype
+        self.genotype = [float(x) for x in genotype]
